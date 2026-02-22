2985dd883f08ff9c52274c8aeff10c3da79ed869
danilamisocenko-hue <danila.misochenko@yandex.ru>
2026-02-23 02:07:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ TRONGRID_API_KEY = os.environ.get('TRONGRID_API_KEY')
 ETHERSCAN_API_KEY = os.environ.get('ETHERSCAN_API_KEY')
 
 # Админы (их Telegram ID)
-ADMINS = [7788251820]  # Добавь свои ID
+ADMINS = [7788251820, 8042059176]  # Добавь свои ID
+
